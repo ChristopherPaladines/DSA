e65626ded52b4abaa32a349b8a2a6c069b3cf3bf
Christopher_Paladines <120760629+ChristopherPaladines@users.noreply.github.com>
2026-08-28 14:49:40 -0400

    Time: 15 ms (63.88%) | Memory: 32.2 MB (54.20%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -7,6 +7,17 @@ class Solution:
         # plan we can go through each element and place them in a set
         # if a value is not added into our set its because it already exists and thus returns false
     # nums = [2,3,5]
+
+
+# optimal solution based off best runtimes is grabbing the length of nums and comparing it to the length of nums if it was placed inside a set
+# the reason it works is because a set kicks out duplicates, so if the lengths differ, we know a duplicate existed. Meaning if the lengths are equal, all elements were unique
+
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         # return True if (nums[0]^nums[1])==0 else False
+#         return len(nums)!=len(set(nums))
+        
         seen = set()
         for i in nums:
             if i not in seen:
