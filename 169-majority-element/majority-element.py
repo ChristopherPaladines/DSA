4581b5ed13ecded891c5234ec6673c15c70218ce
Christopher_Paladines <120760629+ChristopherPaladines@users.noreply.github.com>
2026-09-03 11:19:01 -0400

    Time: 19 ms (6.84%) | Memory: 21 MB (84.24%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,16 +1,27 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority_elem = {}
-        major_count = 0
-        majority_num = 0
-        # size_n = len(nums)/2
-        for index,values in enumerate(nums):
-            majority_elem[values] = majority_elem.get(values, 0) + 1
-            if majority_elem[values] >= major_count:
-                major_count = majority_elem[values]
-                majority_num = values
-            
-            # how do I find the exact key and count and return that value
-            # Find a way to return the key of the highest count.
-        return majority_num
+        # Input: given an arr 'nums' of size  n
+        # Out:   return the majority element, an element that appears more than half the size of n or the array.
+        
+        # We must first think what DSA: would most be beneficial
+        # We must scan through the entire array
+
+        # set: A set wouldnt be helpful since we need to figure out the majority
+        # A hashmap would be the easiest for counting the highest value seen.
+        # once we scan, we must see if the count can be counted and compared to see if it's the highest.
+
+        # if using range we can probably compare 1 + 1
+      dict_ele = {}
+      major_count = 0
+      majority_element = 0
+
+      for index, values in enumerate(nums):
+        dict_ele[values] = dict_ele.get(values, 0) + 1
+        if dict_ele[values] >= major_count:
+            major_count = dict_ele[values]
+            majority_element = values
+      return majority_element
+
+
+        
 
